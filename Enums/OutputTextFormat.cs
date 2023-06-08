e1fe447081e9b0c746842ab2ec8935f98a5f966e
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-06-08 14:38:31 +0300

    Add files via upload

---
+++ Enums/OutputTextFormat.cs
@@ -0,0 +1,18 @@
+﻿
+namespace MyLibraries.MyEncodingLib.Enums
+{
+    /// <summary>
+    /// Формат вихідного тексту
+    /// </summary>
+    public enum OutputTextFormat
+    {
+        /// <summary>
+        /// Шістнадцяткова система числення
+        /// </summary>
+        HEX = 1,
+        /// <summary>
+        /// Шістдесятичетверна система числення
+        /// </summary>
+        Base64 = 2
+    }
+}
---
+++ Enums/TypeEncoding.cs
@@ -0,0 +1,30 @@
+﻿
+namespace MyLibraries.MyEncodingLib.Enums
+{
+    /// <summary>
+    /// Тип кодування
+    /// </summary>
+    public enum TypeEncoding
+    {
+        /// <summary>
+        /// За замовчуванням
+        /// </summary>
+        Default = 0,
+        /// <summary>
+        /// 7-бітне кодування
+        /// </summary>
+        ASCII = 1,
+        /// <summary>
+        /// 8-бітне кодування
+        /// </summary>
+        UTF8 = 2,
+        /// <summary>
+        /// 32-бітне кодування
+        /// </summary>
+        UTF32 = 3,
+        /// <summary>
+        /// 16-бітне кодування
+        /// </summary>
+        Unicode = 4
+    }
+}
