b609f10a538905d78a36323b8177644aa878b5ca
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-06-08 14:37:45 +0300

    Add files via upload

---
+++ Classes/MyByte.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+using MyLibraries.MyEncodingLib.Enums;
+
+namespace MyLibraries.MyEncodingLib.Classes
+{
+    /// <summary>
+    /// Байт
+    /// </summary>
+    static public class MyByte
+    {
+        #region Functions
+        #region Gets
+        /// <summary>
+        /// Отримати байти
+        /// </summary>
+        /// <param name="arrayByte">Поточний масив байтів</param>
+        /// <param name="str">Рядок для отримання байтів</param>
+        /// <param name="typeEncoding">Тип кодування</param>
+        static public void GetBytes(ref byte[] arrayByte, string str, TypeEncoding typeEncoding = TypeEncoding.Default)
+        {
+            switch (typeEncoding)
+            {
+                case TypeEncoding.Default:
+                    {
+                        arrayByte = Encoding.Default.GetBytes(str); return;
+                    }
+                case TypeEncoding.ASCII:
+                    {
+                        arrayByte = Encoding.ASCII.GetBytes(str); return;
+                    }
+                case TypeEncoding.UTF8:
+                    {
+                        arrayByte = Encoding.UTF8.GetBytes(str); return;
+                    }
+                case TypeEncoding.UTF32:
+                    {
+                        arrayByte = Encoding.UTF32.GetBytes(str); return;
+                    }
+                case TypeEncoding.Unicode:
+                    {
+                        arrayByte = Encoding.Unicode.GetBytes(str); return;
+                    }
+            }
+        }
+        /// <summary>
+        /// Отримати байти
+        /// </summary>
+        /// <param name="arrayByte">Поточний масив байтів</param>
+        /// <param name="number">Число для отримання байтів</param>
+        /// <param name="typeEncoding">Тип кодування</param>
+        static public void GetBytes(ref byte[] arrayByte, int number, TypeEncoding typeEncoding = TypeEncoding.Default) =>
+            GetBytes(ref arrayByte, number.ToString(), typeEncoding);
+        #endregion Gets
+        #endregion Functions
+    }
+}
---
+++ Classes/MyHMAC.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+using MyLibraries.MyEncodingLib.Enums;
+
+namespace MyLibraries.MyEncodingLib.Classes
+{
+    /// <summary>
+    /// HMAC
+    /// </summary>
+    static public class MyHMAC
+    {
+        #region Classes
+        /// <summary>
+        /// SHA-256
+        /// </summary>
+        static public class SHA256
+        {
+            /// <summary>
+            /// Зашифрувати (захешувати)
+            /// </summary>
+            /// <param name="hashedStr">Захешований рядок</param>
+            /// <param name="textFormat">Формат вихідного тексту</param>
+            /// <param name="secretKey">Секретний ключ</param>
+            /// <param name="text">Текс для хешування</param>
+            static public void Encrypt(ref string hashedStr, OutputTextFormat textFormat, string secretKey, string text)
+            {
+                var encoding = new ASCIIEncoding();
+                byte[] keyByte = encoding.GetBytes(secretKey);
+                byte[] messageBytes = encoding.GetBytes(text);
+
+                using (var hmacsha256 = new HMACSHA256(keyByte))
+                {
+                    byte[] hashmessage = hmacsha256.ComputeHash(messageBytes);
+
+                    switch (textFormat)
+                    {
+                        case OutputTextFormat.HEX:
+                            {
+                                hashedStr = Convert.ToHexString(hashmessage);
+                                return;
+                            }
+                        case OutputTextFormat.Base64:
+                            {
+                                hashedStr = Convert.ToBase64String(hashmessage);
+                                return;
+                            }
+                    }
+                }
+            }
+        }
+        #endregion Classes
+    }
+}
